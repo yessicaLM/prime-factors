5e1a3388e714041072b4ece76f84d8e009dcc2b8
yessica <yessica@leanmind.es>
2022-07-04 16:02:54 +0100

    REFACTOR primeFactors.ts file

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,7 +1,9 @@
 export const primeFactorsCalculator = (number: number): number[] => {
+  const MINIMUM_NUMBER = 2;
   let minimumDivisor = 2;
-  const listOfFactors = []
-  while (number >= 2) {
+  const listOfFactors = [];
+
+  while (number >= MINIMUM_NUMBER) {
     if(number % minimumDivisor === 0) {
       number = number / minimumDivisor;
       listOfFactors.push(minimumDivisor);
