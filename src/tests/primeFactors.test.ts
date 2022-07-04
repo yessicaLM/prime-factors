8f8234bfcfdbeecf0162ea8aed474fb0daa7e260
yessica <yessica@leanmind.es>
2022-07-04 15:35:20 +0100

    RED: given 2 * 2 returns [2, 2]

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,6 +1,6 @@
 import { primeFactorsCalculator } from '../core/primeFactors';
 
-// 2 -> [2]
+// 2 -> [2] --> DONE
 // 2 * 2 -> [2,2]
 // 2 * 2 * 2 -> [2,2,2]
 // 3 -> [3]
@@ -13,5 +13,9 @@ import { primeFactorsCalculator } from '../core/primeFactors';
 describe('Prime factors', () => {
   it('given 2 returns [2]', () => {
     expect(primeFactorsCalculator(2)).toEqual([2]);
+  });
+
+  it('given 2 * 2 returns [2, 2]', () => {
+    expect(primeFactorsCalculator(2 * 2)).toEqual([2, 2]);
   });
 });
