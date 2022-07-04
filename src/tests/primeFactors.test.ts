5c8621d590545acb960e4c70d76e9a2c20144594
yessica <yessica@leanmind.es>
2022-07-04 16:06:58 +0100

    REFACTOR primeFactors.test file

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,45 +1,59 @@
 import { primeFactorsCalculator } from '../core/primeFactors';
 
-// 2 -> [2] --> DONE
-// 2 * 2 -> [2,2] --> DONE
-// 2 * 2 * 2 -> [2,2,2] --> DONE
-// 3 -> [3] --> DONE
-// 3 * 3 -> [3,3] --> DONE
-// 3 * 2 -> [2,3] --> DONE
-// 5 * 5 -> [5,5] --> DONE
-// 5 * 7 * 11 * 3 -> [3,5,7,11]
-
-
 describe('Prime factors', () => {
   it('given 2 returns [2]', () => {
-    expect(primeFactorsCalculator(2)).toEqual([2]);
+    const currentValue = 2;
+    const expectedValue = 2;
+
+    expect(primeFactorsCalculator(currentValue)).toEqual([expectedValue]);
   });
 
   it('given 2 * 2 returns [2, 2]', () => {
-    expect(primeFactorsCalculator(2 * 2)).toEqual([2, 2]);
+    const currentValue = 2 * 2;
+    const expectedValue = [2, 2];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 2 * 2 * 2 returns [2, 2, 2]', () => {
-    expect(primeFactorsCalculator(2 * 2 * 2)).toEqual([2, 2, 2]);
+    const currentValue = 2 * 2 * 2;
+    const expectedValue = [2, 2, 2];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 3 returns [3]', () => {
-    expect(primeFactorsCalculator(3)).toEqual([3]);
+    const currentValue = 3;
+    const expectedValue = [3];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 3 * 3 returns [3, 3]', () => {
-    expect(primeFactorsCalculator(3 * 3)).toEqual([3, 3]);
+    const currentValue = 3 * 3;
+    const expectedValue = [3, 3];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 3 * 2 returns [2, 3]', () => {
-    expect(primeFactorsCalculator(3 * 2)).toEqual([2, 3]);
+    const currentValue = 3 * 2;
+    const expectedValue = [2, 3];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 5 * 5 returns [5, 5]', () => {
-    expect(primeFactorsCalculator(5 * 5)).toEqual([5, 5]);
+    const currentValue = 5 * 5;
+    const expectedValue = [5, 5];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 
   it('given 5 * 7 * 11 * 3 returns [3,5,7,11]', () => {
-    expect(primeFactorsCalculator(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
+    const currentValue = 5 * 7 * 11 * 3;
+    const expectedValue = [3, 5, 7, 11];
+
+    expect(primeFactorsCalculator(currentValue)).toEqual(expectedValue);
   });
 });
