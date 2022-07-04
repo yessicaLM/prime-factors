cdb82dbf3e3c253a0cc5c4294d580577f5d44713
yessica <yessica@leanmind.es>
2022-07-04 15:52:54 +0100

    GREEN: given 2 * 2 returns [2, 2]

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,3 +1,10 @@
 export const primeFactorsCalculator = (number: number): number[] => {
-  return [2];
+  const listOfFactors = []
+  while (number >= 2) {
+    if(number % 2 === 0) {
+      number = number / 2;
+      listOfFactors.push(2);
+    }
+  }
+  return listOfFactors;
 }
