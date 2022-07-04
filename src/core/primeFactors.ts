efd7a69ea9c3b8edcf93bfca6597161ad5d883eb
yessica <yessica@leanmind.es>
2022-07-04 15:56:50 +0100

    GREEN: given 3 returns [3]

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,9 +1,12 @@
 export const primeFactorsCalculator = (number: number): number[] => {
+  let minimumDivisor = 2;
   const listOfFactors = []
   while (number >= 2) {
-    if(number % 2 === 0) {
-      number = number / 2;
-      listOfFactors.push(2);
+    if(number % minimumDivisor === 0) {
+      number = number / minimumDivisor;
+      listOfFactors.push(minimumDivisor);
+    } else {
+      minimumDivisor ++;
     }
   }
   return listOfFactors;
